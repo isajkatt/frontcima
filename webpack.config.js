416bf1f00c0f683f0fdd60b578fb8fcdedab9b5c
isajkatt <ismagulov.azamat@hotmail.com>
2019-01-08 09:36:24 +0500

    Project setup (express server and webpack configuration)

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    ]
+  }
+}
